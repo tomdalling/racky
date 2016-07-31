86b2c383abcd0f0e06ca284988ea05d58fe6c858
Tom Dalling <tom@tomdalling.com>
2016-07-31 13:07:01 +1000

    Insane bug.
    No idea where the `first` method comes from. It looks like it should be
    `Enumerable#find`, but it doesn't behave that way. In any case, it
    doesn't find rows by id.

--- app/queries/user.rb
+++ app/queries/user.rb
@@ -2,7 +2,7 @@ class Queries::User
   include App::Inject['db']
 
   def find(id)
-    user = db[:users].find(id).first
+    user = db[:users].first(id: id)
     user ? OpenStruct.new(user) : nil
   end
 
