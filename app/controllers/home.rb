e63f5b7a0ff8982190736a5e3fcd187be6259a49
Tom Dalling <tom@tomdalling.com>
2016-08-01 15:32:38 +1000

    Styling homepage

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -1,19 +1,39 @@
+require 'view'
+
 class Controllers::Home
   include App::Inject[
     works: 'queries.work',
-    template: 'templates.home',
-    layout: 'templates.layout',
+    view: 'templates.home',
   ]
 
   def call(env)
+    #TODO: this needs to be moved into the view layer
+    featured = enrich(works.featured)
+    latest = enrich(works.latest)
+
     [200, {}, [
-      layout.render(
-        title: 'Litmal – Share nice readable fiction',
-        content: template.render(
-          featured_work: works.featured,
-          latest_work: works.latest,
-        )
+      View.render(view,
+        featured_work: featured,
+        latest_work: latest,
       )
     ]]
+  end
+
+  def enrich(work)
+    return unless work
+
+    doc = LIF::JSON::Parser.parse(work.lif_document)
+    first_para = doc.scenes.first.paragraphs.first
+
+    blurb_doc = doc.with(scenes: [
+      LIF::Scene.with(paragraphs: [first_para]),
+    ])
+
+    work.blurb_html = LIF::HTMLConverter.convert(blurb_doc)
+
+    # needs the author to generate the path
+    work.path = "/todo/here"
+
+    work
   end
 end
--- test/feature/homepage_test.rb
+++ test/feature/homepage_test.rb
@@ -3,8 +3,8 @@ require 'feature_test'
 class HomepageTest < FeatureTest
   def test_content
     create!(works: [
-      { title: 'Old Mold', featured_at: Time.now - 5 },
-      { title: 'Featured Peatured', featured_at: Time.now },
+      { title: 'Old Mold', published_at: Time.now, featured_at: Time.now - 5 },
+      { title: 'Featured Peatured', published_at: Time.now, featured_at: Time.now },
       { title: 'Latest Baitest', published_at: Time.now + 7 },
     ])
 
