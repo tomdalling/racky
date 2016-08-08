81f68a9b208c21c2494a38d309345dc2633d812b
Tom Dalling <tom@tomdalling.com>
2016-08-08 22:45:28 +1000

    HTTP caching for homepage

--- app/controllers/home.rb
+++ app/controllers/home.rb
@@ -1,5 +1,6 @@
 require 'view'
 require 'work_decorator'
+require 'http_cache'
 
 class Controllers::Home
   include App::Inject[
@@ -9,11 +10,25 @@ class Controllers::Home
 
   def call(env)
     home = query.call
-    [200, {}, [
-      View.render(view,
-        featured_work: WorkDecorator.new(home.featured),
-        latest_work: WorkDecorator.new(home.latest),
-      )
-    ]]
+    HTTPCache.if_none_match(etag(home), env) do
+      [
+        View.render(view,
+          featured_work: WorkDecorator.new(home.featured),
+          latest_work: WorkDecorator.new(home.latest),
+        )
+      ]
+    end
+  end
+
+  # TODO: this is too manual. needs some sort of etag generator
+  #       that is easier to use
+  def etag(home)
+    # TODO: don't use published_at. Use updated_at or something.
+    parts = "controllers.home-"
+    parts << home.featured.id
+    parts << home.featured.published_at.iso8601
+    parts << home.latest.id
+    parts << home.latest.published_at.iso8601
+    Digest::MD5.hexdigest(parts)
   end
 end
