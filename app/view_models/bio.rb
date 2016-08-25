5f0c6bbddc1bf14d0ddad7a94a975f85e4e3062f
Tom Dalling <tom@tomdalling.com>
2016-08-26 00:13:30 +1000

    Author bios

--- app/view_models/bio.rb
+++ app/view_models/bio.rb
@@ -1,3 +1,5 @@
+require 'gravatar'
+
 class ViewModels::Bio
   def initialize(author)
     @author = author
@@ -5,5 +7,25 @@ class ViewModels::Bio
 
   def author
     @author
+  end
+
+  def author_name
+    @author.name
+  end
+
+  def animated
+    false #TODO: here
+  end
+
+  def gravatar_url
+    Gravatar.url(author.email)
+  end
+
+  def website
+    'http://example.com' #TODO: here
+  end
+
+  def twitter_username
+    'example' #TODO: here
   end
 end
---
+++ lib/gravatar.rb
@@ -0,0 +1,21 @@
+module Gravatar
+  DEFAULT_OPTIONS = {
+    size: 200,
+    default: 'mm',
+  }
+
+  def self.url(email, options={})
+    options = DEFAULT_OPTIONS.merge(options)
+    params = {
+      s: Integer(options.fetch(:size)),
+      d: CGI.escape(options.fetch(:default)),
+    }
+    query_string = params.any? ?
+       '?' + params.map{ |k, v| "#{k}=#{v}" }.join('&') :
+       ''
+
+    hash = Digest::MD5.hexdigest(email.downcase)
+
+    "https://secure.gravatar.com/avatar/#{hash}#{query_string}"
+  end
+end
